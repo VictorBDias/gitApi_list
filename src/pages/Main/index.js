7deed16e85b267e2be6d6d35ff7aa1c5cc556034
VictorBDias <victor.batisttete@gmail.com>
2020-11-25 12:53:20 -0300

    :sparkles: validation fields + feedback messages

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { Container, Form, SubmitButton, List } from './styles';
 
@@ -11,14 +14,39 @@ import api from '../../services/api';
 export default function Main() {
   const [repositories, setRepositories] = useState([]);
   const [input, setInput] = useState('');
-  // const [loading, setLoading] = useState();
+  const notifySuccess = () => toast.dark('Repository successfully added!');
+  const notifyNotFound = () =>
+    toast.error('Not found, double check if the repository name is correct 😥');
+  const notifyDuplicate = () =>
+    toast.error(
+      'We found a duplicate, check the repository list if you think this is a error'
+    );
 
   async function handleSubmit(e) {
+    let erro = '';
     e.preventDefault();
-    await api.get(`https://api.github.com/repos/${input}`).then((res) => {
-      const newRepo = res.data;
-      setRepositories([...repositories, newRepo]);
-    });
+    try {
+      erro = 'notFound';
+      const hasRepo = repositories.find((repo) => repo.full_name === input);
+      if (hasRepo) {
+        erro = 'duplicate';
+        throw Error();
+      }
+      document.querySelector('#repoId').style.borderColor = 'rgb(51,51,51)';
+      await api.get(`https://api.github.com/repos/${input}`).then((res) => {
+        notifySuccess();
+        const newRepo = res.data;
+        setRepositories([...repositories, newRepo]);
+      });
+    } catch (error) {
+      if (erro === 'notFound') {
+        notifyNotFound();
+      } else if (erro === 'duplicate') {
+        notifyDuplicate();
+      }
+      document.querySelector('#repoId').style.borderColor =
+        'rgb(240, 135, 135)';
+    }
   }
 
   useEffect(() => {
@@ -32,6 +60,7 @@ export default function Main() {
 
   return (
     <Container>
+      <ToastContainer />
       <h1>
         <FaGithub />
         Repositories
@@ -40,6 +69,7 @@ export default function Main() {
       <Form onSubmit={handleSubmit}>
         <input
           type="text"
+          id="repoId"
           placeholder="Add repository"
           onChange={(e) => setInput(e.target.value)}
         />
